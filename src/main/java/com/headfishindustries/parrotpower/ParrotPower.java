06fe3a0c0e7e75818ecef1f856beb144779881fc
TheIcyOne <icy.headfishindustries@gmail.com>
2017-06-19 21:57:01 +0100

    init

---
+++ src/main/java/com/headfishindustries/parrotpower/ParrotPower.java
@@ -0,0 +1,9 @@
+package com.headfishindustries.parrotpower;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=ParrotPower.MODID, version=ParrotPower.VERSION, canBeDeactivated=true)
+public class ParrotPower {
+	public static final String MODID = "parrotpower";
+	public static final String VERSION = "GRADLE:VERSION" + "GRADLE:BUILD";
+}
